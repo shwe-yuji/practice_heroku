419d694b41c86238044ca35eb5bb828f949eecae
shwe-yuji <okumura19943210yuji@gmail.com>
2020-08-16 08:01:36 +0900

    DB 完了

---
+++ db/migrate/20200815225223_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title, null: false
+      t.integer :price, null: false
+      t.text :memo, null: false
+      t.timestamps
+    end
+  end
+end
