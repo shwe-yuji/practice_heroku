73de400f5986ad006cfa8d9df3118ae489eed93e
shwe-yuji <okumura19943210yuji@gmail.com>
2020-08-16 20:52:29 +0900

    seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create!(
+  title: "鬼滅の刃",
+  price: "390",
+  memo: "無惨様"
+)
